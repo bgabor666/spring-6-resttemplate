0e3ff249f0245acc9378f4d7ea708f6421ab2564
Gabor Ballabas <gbr666@gmail.com>
2023-05-11 11:54:22 +0200

    Add HTTP Basic security config

--- src/main/java/guru/springframework/spring6resttemplate/config/RestTemplateBuilderConfig.java
+++ src/main/java/guru/springframework/spring6resttemplate/config/RestTemplateBuilderConfig.java
@@ -16,6 +16,12 @@ public class RestTemplateBuilderConfig {
     @Value("${rest.template.rootUrl}")
     String rootUrl;
 
+    @Value("${rest.template.username}")
+    String username;
+
+    @Value(("${rest.template.password}"))
+    String password;
+
     @Bean
     RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer){
 
@@ -25,6 +31,7 @@ public class RestTemplateBuilderConfig {
         DefaultUriBuilderFactory uriBuilderFactory = new
                 DefaultUriBuilderFactory(rootUrl);
 
-        return builder.uriTemplateHandler(uriBuilderFactory);
+        return builder.uriTemplateHandler(uriBuilderFactory)
+                .basicAuthentication(username, password);
     }
 }
